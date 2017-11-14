c9ed6b63e50df74d32b6c87ef69a18f27187b373
LEEChangGun <cd8114@gmail.com>
2017-11-15 03:32:08 +0900

    adding code in app.js

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const event = require('./routes/event');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/event',event);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
