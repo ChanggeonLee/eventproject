ac2c8487d574e49420436986b054b2977b5a2260
LEEChangGun <cd8114@gmail.com>
2017-11-16 11:49:44 +0900

    edit post form for user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const catchErrors = require('../lib/async-error');
+const User = require('../model/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+
+// 회원 가입 정보를 디비에 저장한다
+router.post('/',(req,res,next)=>{
+
+});
+
+// 에러 검사를 위해 catchErrors
+router.get('/new',(req,res,next)=>{
+  // 회원 가입 정보를 검사를 한다.
+  // 예외처리를하고
+  // 이미 사용하고있는 사용자일 경우에도 에러처리를 한다.
+  // 새로운 사용자를 디비에 저장 한다
+  // 저장시에 비밀번호는 암호화허여 저장한다.
+  // 홈 화면으로 리다이렉트 해준다~
+  res.render('users/new');
 });
 
 module.exports = router;
