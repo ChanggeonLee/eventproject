b01ff2d37a6fc25ffbb774d666a0a1eef53e15ed
LEEChangGun <cd8114@gmail.com>
2017-11-22 16:50:53 +0900

    event index

--- models/event.js
+++ models/event.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
 const Schema = mongoose.Schema;
 
 var schema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: 'User' },
-    name: {type: String, required: true, trim: true},
+    title: {type: String, required: true, trim: true},
     locate: {type: String, required: true, trim: true},
     start_time: {
         date:{type: String, required: true, trim: true},
@@ -19,15 +20,19 @@ var schema = new Schema({
     event_type: {type: String, required: true, trim: true},
     event_field: {type: String, required: true, trim: true},
     ticket: {
-        name: {type: String, required: true, trim: true },
-        free: {type: Boolean, required: true, trim: true ,default: true},
-        cost: {type: String, required: true, trim: true }
+        name: {type: String},
+        free: {type: Boolean, default: true},
+        cost: {type: String}
     },
     attendance_max: {type:Number, required: true, trim: true},
-    attendance: {type:Number, required: true, trim: true, default: 0},
     // 이미지가 들어가는 스키마가 필요함
-    img_url:[String],
-    img_key:[String],
+    // img_url:[String],
+    // img_key:[String],
+    // 좋아요 ,  응답 , 읽은 사람수
+    numAttendance: {type:Number, default: 0},
+    numLikes: {type: Number, default: 0},
+    numAnswers: {type: Number, default: 0},
+    numReads: {type: Number, default: 0},
     // 설문에 필요한 스키마를 추가할 거임
     createdAt: {type: Date, default: Date.now}
 }, {
@@ -35,6 +40,7 @@ var schema = new Schema({
   toObject: {virtuals: true}
 });
 
+schema.plugin(mongoosePaginate);
 var Event = mongoose.model('Event', schema);
 
 module.exports = Event;
--- routes/event.js
+++ routes/event.js
@@ -4,17 +4,6 @@ const catchErrors = require('../lib/async-error');
 var Event = require('../models/event');
 var User = require('../models/user');
 
-// 파일을 제어 하는 모듈
-var formidable = require('formidable');
-var AWS = require('aws-sdk');
-AWS.config.region = 'ap-northeast-2';
-
-AWS.config.update({
-    accessKeyId: process.env.AWS_KEY,
-    secretAccessKey: process.env.AWS_SECRET,
-    region: 'ap-northeast-2'
-});
-
 function needAuth(req, res, next) {
     if (req.isAuthenticated()) {
       next();
@@ -25,7 +14,23 @@ function needAuth(req, res, next) {
 }
 
 router.get('/', catchErrors( async(req, res, next) => {
-  res.render('index');
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  var query = {};
+  const term = req.query.term;
+  if (term) {
+    query = {$or: [
+      {title: {'$regex': term, '$options': 'i'}}
+    ]};
+  }
+  const events = await Event.paginate(query, {
+    sort: {createdAt: -1}, 
+    populate: 'author', 
+    page: page, limit: limit
+  });
+  res.render('event/index', {events: events, term: term, query: req.query});
+  
 }));
 
 router.get('/newevent', needAuth , catchErrors( async(req, res, next)=> {
@@ -34,85 +39,40 @@ router.get('/newevent', needAuth , catchErrors( async(req, res, next)=> {
 
 router.post('/:id', needAuth, catchErrors( async(req, res, next)=> {
   
-  // // 기본 데이터 설정
-  // event = new Event({
-  //   author: req.user.id,
-  //   name: req.body.name,
-  //   locate: req.body.locate,
-  //   info: req.body.info,
-  //   organize: req.body.organize,
-  //   organize_info: req.body.organize_info,
-  //   event_type: req.body.event_type,
-  //   event_field: req.body.event_field,
-  //   attendance_max: req.body.attendance_max,
-  // });
-
-  // // 시작 시간 끝시간 설정
-  // event.start_time.date = req.body.start_time_date;
-  // event.start_time.time = req.body.start_time_time;
-  // event.end_time.date = req.body.end_time_date;
-  // event.end_time.time = req.body.end_time_date;
-  
-  // // 티켓 요금 설정
-  // if(req.body.ticket_name){
-  //   // 유료 티켓
-  //   event.ticket.free = false;
-  //   event.ticket.name = req.body.ticket_name;
-  //   event.ticket.cost = req.body.ticket_price
-  // }
-
-  //else 무료 티켓
-  // console.log(event);
-  // console.log(req.body);
-
-  // console.log(req.body.images);
-  // console.log(req.body.images));
-
-  // // 이미지 저장
-  var form = new formidable.IncomingForm();
-  // form.parse(req, function(err, fields, files){
-  //   if(err){
-  //     console.log(err);
-  //   }
-  //   var url;
-  //   // const S3_Bucket = 'wpclassevent';
-  //   const img_name = files.images;
-  //   const UserId = req.user.id;
-  //   console.log(files);
-  //   console.log(fields);
-  // });
-  form.multiples = true;
-  form.parse(req, function(err, fields, files){
-    console.log(files);
-    var url;
-    // const S3_Bucket = 'jibang';
-    // const img_name = files.roomimg.name;
-    // const UserId = req.user.id;
-
-    // if(img_name != ''){
-    //     // 이미지 업로드
-    //     var s3 = new AWS.S3();
-    //     var params = {
-    //          Bucket: S3_Bucket,
-    //          Key:img_name,
-    //          ACL:'public-read',
-    //          Body: require('fs').createReadStream(files.roomimg.path)
-    //     }
-    //     s3.upload(params, function(err, data){
-    //          var result='';
-    //          if(err)
-    //             // result = 'Fail';
-    //             console.log(err);
-    //          else
-    //             console.log(data);
-    //     });
-    //     // 업로드 된 이미지 URL가지고 오기
-    //     url = `https://${S3_Bucket}.s3.amazonaws.com/${img_name}`;    
-    // }else{
-    //     url = "#";
-    // }
+  // 기본 데이터 설정
+  event = new Event({
+    author: req.user.id,
+    name: req.body.name,
+    locate: req.body.locate,
+    info: req.body.info,
+    organize: req.body.organize,
+    organize_info: req.body.organize_info,
+    event_type: req.body.event_type,
+    event_field: req.body.event_field,
+    attendance_max: req.body.attendance_max,
   });
-  // res.redirect('back');
+
+  // 시작 시간 끝시간 설정
+  event.start_time.date = req.body.start_time_date;
+  event.start_time.time = req.body.start_time_time;
+  event.end_time.date = req.body.end_time_date;
+  event.end_time.time = req.body.end_time_date;
+  
+  // 티켓 요금 설정
+  if(req.body.ticket_name){
+    // 유료 티켓
+    event.ticket.free = false;
+    event.ticket.name = req.body.ticket_name;
+    event.ticket.cost = req.body.ticket_price
+  }
+
+  // 이미지 저장
+  // 설문
+  await event.save();
+
+  req.flash('success', '이벤트 생성 완료~');
+
+  res.redirect('/');
 }));
 
 module.exports = router;
